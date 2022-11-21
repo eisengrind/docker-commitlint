27fb1d30e5672f7707706cdc7dff15c6ea0823cc
pixlcrashr <agpheaddikilla@yahoo.de>
2022-11-21 23:09:04 +0100

    ci: fix docker tag

--- .github/scripts/push.sh
+++ .github/scripts/push.sh
@@ -3,7 +3,7 @@
 HASH=$(echo $(date --iso-8601=minutes) | sha1sum | head -c 40)
 SHORT_HASH=$(echo $HASH | cut -c1-8)
 
-docker build -f Dockerfile . $IMAGE:latest
+docker build -f Dockerfile . -t $IMAGE:latest
 
 docker tag $IMAGE:latest $IMAGE:$HASH
 docker tag $IMAGE:latest $IMAGE:$SHORT_HASH
